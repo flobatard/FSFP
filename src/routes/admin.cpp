bd6e77b70f8b064845070debf430a2e21e64c539
flobatard <fb.batard@gmail.com>
2024-11-24 17:02:33 +0100

    All rotes done

--- src/routes/admin.cpp
+++ src/routes/admin.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int admin_routes(FSFP_APP_TYPE& app)
 {   
-    CROW_ADMIN_ROUTE(app, "/admin/owner/<string>").methods(crow::HTTPMethod::Put)([](const crow::request& req, crow::response& res, string owner){
+    CROW_ADMIN_ROUTE(app, "/admin/owner/<string>").methods(crow::HTTPMethod::PUT)([](const crow::request& req, crow::response& res, string owner){
         crow::json::rvalue rjson_body = crow::json::load(req.body.c_str(), req.body.size());
         fsfp::db::owner_metadata owner_m;
         if (!rjson_body.has("max_data_size") || !(rjson_body["max_data_size"].nt() == crow::json::num_type::Unsigned_integer))
@@ -44,7 +44,7 @@ int admin_routes(FSFP_APP_TYPE& app)
         res.end(wjson_body.dump());
     });
 
-    CROW_ADMIN_ROUTE(app, "/admin/owner/<string>").methods(crow::HTTPMethod::Get)([](const crow::request&, crow::response& res, string owner){
+    CROW_ADMIN_ROUTE(app, "/admin/owner/<string>").methods(crow::HTTPMethod::GET)([](const crow::request&, crow::response& res, string owner){
         DatabasesRegistry* db_registry = DatabasesRegistry::GetInstance();
         LMDBWrapper* lmdb = db_registry->getRegistryDatabase();
 
@@ -65,6 +65,39 @@ int admin_routes(FSFP_APP_TYPE& app)
         res.set_header("Content-Type", "application/json");
         res.end(wjson_body.dump());
     });
+
+    CROW_ADMIN_ROUTE(app, "/admin/owner/<string>").methods(crow::HTTPMethod::DELETE)([](const crow::request&, crow::response& res, string owner){
+        DatabasesRegistry* db_registry = DatabasesRegistry::GetInstance();
+        LMDBWrapper* lmdb = db_registry->getRegistryDatabase();
+
+        int rc = fsfp::db::owner_del(lmdb, owner);
+        if (rc){
+            res.code = 404;
+            res.end();
+            return;
+        }
+        
+        res.code = 200;
+        res.end();
+    });
+
+
+    CROW_ADMIN_ROUTE(app, "/admin/owners/all").methods(crow::HTTPMethod::GET)([](const crow::request&, crow::response& res){
+        DatabasesRegistry* db_registry = DatabasesRegistry::GetInstance();
+        LMDBWrapper* lmdb = db_registry->getRegistryDatabase();
+
+        std::vector<std::string> owners = lmdb->keys();
+
+        crow::json::wvalue wjson_body;
+        wjson_body["owners"] = owners;
+        
+        res.code = 200;
+        res.set_header("Content-Type", "application/json");
+        res.end(wjson_body.dump());
+    });
+
+
+
 
 
     return 0;
